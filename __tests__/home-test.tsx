6603ab44f12a9fa9ba265649309385ef5f6b43ac
Faysal Bsata <faysalbsata@hotmail.com>
2022-06-26 14:27:04 +0400

    testing setup

--- __tests__/home-test.tsx
+++ __tests__/home-test.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import App from '../App';
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
 test('renders correctly', () => {
+  jest.mock(
+    '@react-native-async-storage/async-storage',
+    () => mockAsyncStorage,
+  );
   const tree = renderer.create(<App />).toJSON();
   expect(tree).toMatchSnapshot();
 });
